3788d07ef78d8060a7c889863d5f8f6ecb9fc664
javier-capobianco <javier.capobianco.2210@gmail.com>
2025-11-10 03:34:41 -0800

    feat: all service cards in home page also direct to appointments page

--- src/components/home/service-card.tsx
+++ src/components/home/service-card.tsx
@@ -13,21 +13,23 @@ export default function ServiceCard({ serviceName, imageName }: ServiceCardProps
             />
             <h3 className="font-bold text-lg mb-2">{serviceName}</h3>
             <div className="absolute bottom-4 right-4">
-                <button className="bg-red-600 p-3 rounded-full shadow-md hover:bg-red-700">
-                    <svg
-                        className="w-4 h-4 text-white"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M17 8l4 4m0 0l-4 4m4-4H3"
-                        />
-                    </svg>
-                </button>
+                <a href="/appointments">
+                    <button className="bg-red-600 p-3 rounded-full shadow-md hover:bg-red-700">
+                        <svg
+                            className="w-4 h-4 text-white"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M17 8l4 4m0 0l-4 4m4-4H3"
+                            />
+                        </svg>
+                    </button>
+                </a>
             </div>
         </div>
     );
